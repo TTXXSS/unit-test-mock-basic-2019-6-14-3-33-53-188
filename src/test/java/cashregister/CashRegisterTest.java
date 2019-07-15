4d47ec5b8990bdc7751488f5c6f1f804611681a1
TTXXSS <1374230455@qq.com>
2019-07-15 17:40:04 +0800

    add:CashRegister method print_the_stub_purchase_when_call_process and  verify_with_process_call_with_mockito

--- src/test/java/cashregister/CashRegisterTest.java
+++ src/test/java/cashregister/CashRegisterTest.java
@@ -11,22 +11,41 @@ public class CashRegisterTest {
     @Test
     public void should_print_the_real_purchase_when_call_process() {
         //given
+        Printer printer = mock(Printer.class);
+        Item[] item = new Item[1];
+        item[0] = new Item("枕套", 20.0);
+        Purchase purchase = new Purchase(item);
+        CashRegister cashRegister = new CashRegister(printer);
         //when
+        cashRegister.process(purchase);
         //then
+        verify(printer,times(1)).print("枕套\t20.0\n");
     }
 
     @Test
     public void should_print_the_stub_purchase_when_call_process() {
         //given
+        Printer printer = mock(Printer.class);
+        Purchase purchase = mock(Purchase.class);
+        CashRegister cashRegister = new CashRegister(printer);
+        when(purchase.asString()).thenReturn("Hello");
         //when
+        cashRegister.process(purchase);
         //then
+        verify(printer, never()).print("");
+        verify(printer, times(1)).print("Hello");
     }
 
     @Test
     public void should_verify_with_process_call_with_mockito() {
         //given
+        Printer printer = mock(Printer.class);
+        CashRegister cashRegister = new CashRegister(printer);
+        Purchase purchase = mock(Purchase.class);
         //when
+        cashRegister.process(purchase);
         //then
+        verify(printer).print(purchase.asString());
     }
 
 }
